d0976af8839f85802b976b637b33b3b2aa19a3db
nguyenhuudinh <dinhnguyenhuu1995@gmail.com>
2018-03-15 20:27:51 -0700

    Add an full_title helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello 
-  	render html: "hello world! I am a RoR newbie"
+  	render html: "Hello world! I am a RoR newbie"
   end
 end
