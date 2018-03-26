773c68088a7b10e7b7218d0e673ee9f7d84d7143
nguyenhuudinh <dinhnguyenhuu1995@gmail.com>
2018-03-26 11:29:15 +0700

    remove byebug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,6 @@ class UsersController < ApplicationController
    private
 
    	def user_params
-   		byebug
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
    	end 
 
