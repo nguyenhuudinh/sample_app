ab32a83ff5102b8bcc1bb620dcab7da9577488b9
nguyenhuudinh <dinhnguyenhuu1995@gmail.com>
2018-03-28 22:20:35 +0700

    Add signup page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
   def new
-  	@user = User.new
+  end
+
+  def create
   end
 
   def login
   end
 
-  def logout
+  def show
   end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,9 +5,6 @@ class StaticPagesController < ApplicationController
   def help
   end
 
-  def login
-  end
-
   def about
   end
 
