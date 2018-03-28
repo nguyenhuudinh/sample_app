cf53fe71ceca4fab1ad273fa579caf24406364ae
nguyenhuudinh <dinhnguyenhuu1995@gmail.com>
2018-03-28 23:29:44 +0700

    implement basic login

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get sessions_login_url
+    get sessions_login_path
     assert_response :success
   end
 
   test "should get create" do
-    post sessions_login_url
+    post sessions_login_path
     assert_response :success
   end
 
   test "should get destroy" do
-    get sessions_logout_url
+    get sessions_logout_path
     assert_response :success
   end
 
