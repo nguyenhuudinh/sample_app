6eb499d15c0abc75fd2786d166d14a49fe4d8e74
nguyenhuudinh <dinhnguyenhuu1995@gmail.com>
2018-05-18 15:33:10 +0700

    .

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -27,24 +27,24 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                                          password:              "foobar",
                                          password_confirmation: "foobar" } }
     end
-    # assert_equal 1, ActionMailer::Base.deliveries.size
-    # user = assigns(:user)
-    # assert_not user.activated?
-    # # Try to log in before activation.
-    # log_in_as(user)
-    # assert_not is_logged_in?
-    # # Invalid activation token
-    # get edit_account_activation_path("invalid token", email: user.email)
-    # assert_not is_logged_in?
-    # # Valid token, wrong email
-    # get edit_account_activation_path(user.activation_token, email: 'wrong')
-    # assert_not is_logged_in?
-    # # Valid activation token
-    # get edit_account_activation_path(user.activation_token, email: user.email)
-    # assert user.reload.activated?
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    # Try to log in before activation.
+    log_in_as(user)
+    assert_not is_logged_in?
+    # Invalid activation token
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # Valid token, wrong email
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # Valid activation token
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert_not flash.FILL_IN
-    # assert is_logged_in?
+    assert is_logged_in?
   end
 end
